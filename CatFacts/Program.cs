8b885195464eee38f976f48f49eb0f421d3d04f7
Sosou222 <szym.baran222@gmail.com>
2024-12-06 14:42:28 +0100

    added a simple loop

--- CatFacts/Program.cs
+++ CatFacts/Program.cs
@@ -9,13 +9,41 @@
 
             ConnectionClient client = new ConnectionClient();
 
+            bool breakOut = false;
+            while(true)
+            {
+                Console.WriteLine("Press 1 to get a new fact 0 to finish program");
+
+                switch (Console.ReadLine())
+                {
+                    case "0":
+                        breakOut = true;
+                        break;
+
+                    case "1":
+                        GetFacts(client);
+                        break;
+
+                    default:
+                        break;
+                }
+
+                if(breakOut)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Program has ended");
+        }
+
+        private static void GetFacts(ConnectionClient client)
+        {
             var fact = client.GetRandomFact();
             Console.WriteLine(fact.ToString());
 
             FileManager.Write(fact.Fact);
             FileManager.DebugReadFile();
-
-            
         }
     }
 }
